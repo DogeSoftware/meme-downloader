f80b8dee5d6ecb12772ee02781577602592d0b86
Greg <37777903+PhoenixDiscord@users.noreply.github.com>
2018-12-07 14:21:24 +0100

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo Welcome to Module Installer for Meme downloader.
+echo Make sure you have node.js and npm installed!
+echo After this window closes, run the download.cmd file.
+pause
+npm i fs image-download image-type memejs
+echo Everything installed - run download.cmd to start the memery rampage!
+pause
