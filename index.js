1af0c351c95076ff92ef1f78231e627143264cbd
Quin <cheesits456@gmail.com>
2019-10-02 10:51:44 -0700

    changed how indexxing the memes works

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const imageDownload = require('image-download');
 const imageType = require('image-type');
 const meme = require("memejs-c456");
+const highestNumber = parseInt(fs.readFileSync("../HighestNumber.txt", "utf8"));
 
 meme((data, err) => {
   if (!err && data && data.url) {
     imageDownload(data.url[0]).then(buffer => {
       const type = imageType(buffer);
-      fs.writeFileSync(`${fs.readdirSync("./").length + 1}.${type.ext}`, buffer, "utf8"); 
-	  }).catch(err => {});
+      fs.writeFileSync(`${highestNumber + 1}.${type.ext}`, buffer, "utf8");
+      fs.writeFileSync("../HighestNumber.txt", highestNumber + 1, "utf8");
+		}).catch(err => {});
   }
 });
