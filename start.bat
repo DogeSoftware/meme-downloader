b64839455a33e70d10906004550d66e1afb952ab
Quin <cheesits456@gmail.com>
2019-10-02 14:25:54 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,28 +1,27 @@
 @echo off
 mode con: cols=90 lines=26
 title Meme Downloader
-call node scripts/writeHeader.js
 where npm.cmd >nul 2>&1 && goto NPMInstalled || goto NPMNotInstalled
 :NPMInstalled
 where node.exe >nul 2>&1 && goto NodeInstalled || goto NodeNotInstalled
 :NodeInstalled
-echo   Updating Libraries . . .
+echo Updating Libraries . . .
 echo.
 call npm i image-download image-type memejs-c456 colors
 title Meme Downloader
-echo   Libraries are up to date!
+echo Libraries are up to date!
 echo.
-echo   Press any key to continue . . .
+echo Press any key to continue . . .
 pause>nul
 if not exist memes mkdir memes
 cd memes
 cls
 call node ../scripts/writeHeader.js
 echo.
-echo       Close this window at any time to stop the download process
+echo     Close this window at any time to stop the download process
 echo.
 echo.
-echo       Press any key to begin downloading . . .
+echo     Press any key to begin downloading . . .
 pause>nul
 
 :loop
