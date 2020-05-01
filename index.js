9c2bcf5fa87ed9ebe935f1a2ae52cf5a6b7cfd52
Greg <37777903+PhoenixDiscord@users.noreply.github.com>
2020-05-01 17:22:37 +0000

    Broken rewrite

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 const imageDownload = require('image-download');
 const imageType = require('image-type');
-const meme = require("memejs-c456");
+const randomPuppy = require("random-puppy");
 const highestNumber = parseInt(fs.readFileSync("../HighestNumber.txt", "utf8"));
+const subReddits = ['dankmemes', 'meme', 'me_irl', 'crappydesign'];
+const random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
-meme((data, err) => {
-  if (!err && data && data.url) {
-    imageDownload(data.url[0]).then(buffer => {
+randomPuppy(random)
+  .then(url => {
+    imageDownload(url).then(buffer => {
       const type = imageType(buffer);
       fs.writeFileSync(`${highestNumber + 1}.${type.ext}`, buffer, "utf8");
       fs.writeFileSync("../HighestNumber.txt", highestNumber + 1, "utf8");
-		}).catch(err => {});
-  }
-});
+    }).catch(err => { });
+  });
+
